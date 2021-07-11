e10b4f9644ad8ee966c30c608c0de4bb2100be36
Sahil Kaling <sahil@example.com>
2021-07-11 15:55:42 +0530

    user register done

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const connectDB = require('./config/db');
 //Connect Database
 connectDB();
 
+//Init middleware
+app.use(express.json({extended:false}));
+
 
 app.get('/', (req,res) => res.json({msg: 'Welcome to Api'}));
 
