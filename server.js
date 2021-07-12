80c432c674c3fc041377c5287aa72b3537d46031
Sahil Kaling <sahil@example.com>
2021-07-12 21:44:47 +0530

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path')
 
 //Connect Database
 connectDB();
@@ -9,11 +10,17 @@ connectDB();
 app.use(express.json({extended:false}));
 
 
-app.get('/', (req,res) => res.json({msg: 'Welcome to Api'}));
+
 
 app.use('/api/users',require('./routes/users'))
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/contacts',require('./routes/contacts'))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res) => res.sendFile(path.resolve(_dirname,'client','build','index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
